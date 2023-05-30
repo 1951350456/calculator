e854a22f805e7245391e21f99274cd027ee68630
ZZZZYF_Coder_Joshua <115992637+1951350456@users.noreply.github.com>
2023-05-30 12:50:41 +0800

    Add files via upload

---
+++ calculater/main.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+double cal(double m,double n,char op){
+    switch (op) {
+        case '+':
+            return m+n;
+        case '-':
+            return m-n;
+        case '*':
+            return m*n;
+        default:
+            return m/n;
+    }
+}
+void Calculator() {
+    map<char, double> w = {{'+', 1},{'-', 1},{'*',2},{'/',2},{'(',3},{')',3}};
+    stack<char> Operator;
+    stack<double> num;
+    string exp;
+    getline(cin,exp);
+    for(int i=0;i<exp.length();i++) {
+        if(isdigit((exp[i])))
+            num.push(exp[i]-'0');
+        else{
+            if(i==1)
+            Operator.push(exp[i]);
+            else{
+                if(w[Operator.top()]<w[exp[i]])
+                    Operator.push(exp[i]);
+                else{
+                    if(Operator.top()=='(')
+                        Operator.push(exp[i]);
+                    else if(Operator.top()==')'){
+                        Operator.pop();
+                        while(Operator.top()!='('){
+                            double m = num.top();
+                            num.pop();
+                            double n = num.top();
+                            num.pop();
+                            char op = Operator.top();
+                            Operator.pop();
+                            m = cal(n,m,op);
+                            num.push(m);
+                        }
+                        Operator.pop();
+                        Operator.push(exp[i]);
+                    }
+                    else{
+                        double m = num.top();
+                        num.pop();
+                        double n = num.top();
+                        num.pop();
+                        char op = Operator.top();
+                        Operator.pop();
+                        m = cal(n,m,op);
+                        num.push(m);
+                        Operator.push(exp[i]);
+                    }
+                }
+            }
+        }
+    }
+    if(Operator.top()==')') {
+        Operator.pop();
+        while (Operator.top() != '(') {
+            double m = num.top();
+            num.pop();
+            double n = num.top();
+            num.pop();
+            char op = Operator.top();
+            Operator.pop();
+            m = cal(n, m, op);
+            num.push(m);
+        }
+        Operator.pop();
+    }
+    while(!Operator.empty()){
+        double m = num.top();
+        num.pop();
+        double n = num.top();
+        num.pop();
+        char op = Operator.top();
+        Operator.pop();
+        m = cal(n,m,op);
+        num.push(m);
+    }
+    cout<<num.top();
+}
+int main(){
+    Calculator();
+}
